ac985e0e2b7b579e67f7d469db8845386d87b0fb
Mbahena212 <Miguelitobahena@gmail.com>
2025-12-08 19:35:30 +0000

    loop

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,37 @@
 Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
 While loops  
+
+# you can iterate over a range, strings, sequence, etc\
+
+
+for x in reversed(1,11, 2)
+    print(x)
+
+
+print( "Happy new year!")
+
+
+for x in range(1,21):
+    if x = 13:
+        continue
+    else: 
+        print(x)
+
+# while loop = execute some code while some condition remains ture
+
+name = input("enter your name")
+
+while name == "":
+    print("you did not enter your name")
+    name = input("enter your name: ")
+    print(f"Hello {name}")
+
+
+age = int(input(" enter you age"))
+
+
+while age < 0 :
+    print("age cant be negative")
+    age = int(input("enter your age :"))
+
+print()
